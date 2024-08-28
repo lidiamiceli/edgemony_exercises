cc8120940d912cc507d961ca3a8296cb6d653cab
lidiamiceli <miceli.lidia1996@gmail.com>
2024-08-28 10:52:40 +0200

    update style and cards

--- 27-08-24/app/page.tsx
+++ 27-08-24/app/page.tsx
@@ -1,33 +1,57 @@
 import React from 'react';
+import Header from './Header';
 import Card from './components/Card';
 
 const Home: React.FC = () => {
   const cardsData = [
     {
-      title: 'How to position your furniture for positivity',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...',
-      imageUrl: 'https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      title: 'Lo Splendore del Teatro di Eraclea Minoa',
+      description: 'Scopri il teatro: testimonianza della cultura greca in Sicilia',
+      imageUrl: 'https://livingagrigento.it/e/pp/be/upload/img/0ZogS_Eraclea_Minoa_teatro_precedente_struttura_protettiva.jpg',
       date: '2022-10-10',
     },
     {
-      title: 'Matching colorful cushions with sofas',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...',
-      imageUrl: 'https://images.unsplash.com/photo-1567016432779-094069958ea5?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      title: 'Il Quartiere Ellenistico di Eraclea Minoa',
+      description: 'Scopri l\'affascinante quartiere ellenistico di Eraclea Minoa',
+      imageUrl: 'https://projetcefel.eu/wp-content/uploads/2022/04/sito_eraclea_02.jpg',
       date: '2022-11-15',
     },
     {
-      title: 'The best houseplants',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...',
-      imageUrl: 'https://plus.unsplash.com/premium_photo-1679921582421-f770b37cd4d5?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      title: 'Maschere Antiche: Volti del Teatro Greco',
+      description: 'Esplora le affascinanti maschere teatrali dell\'antica Eraclea Minoa',
+      imageUrl: 'https://media-cdn.tripadvisor.com/media/photo-s/1a/1c/19/29/archaeological-museum.jpg',
+      date: '2022-11-15',
+    },
+    {
+      title: 'Vasi Greci di Eraclea Minoa: Arte Antica Svelata',
+      description: 'Esplora l\'eleganza e la storia dei vasi greci antichi',
+      imageUrl: 'https://www.balarm.it/cache/8/6/b/f/e/86bfea9ce1cb202f65de836ecc13c9affb1a4ff6-eraclea-minoa-antiquarium-jpg-59395-1678294410.jpeg',
+      date: '2022-11-15',
+    },
+    {
+      title: 'Eraclea Minoa, la scogliera e le spiagge di Capo Bianco',
+      description: 'Scogliera bianca e panorami mozzafiato sulla costa agrigentina',
+      imageUrl: 'https://ripost.it/wp-content/uploads/2017/07/Capo-Bianco-sentiero.jpg',
+      date: '2022-11-15',
+    },
+    {
+      title: 'Riserva naturale della foce del fiume Platani',
+      description: 'Esplora la bellezza unica della riserva con flora e fauna ricca',
+      imageUrl: 'https://www.cattolicaeracleaonline.it/wp-content/uploads/2018/07/foce-750x410.jpg',
       date: '2022-11-15',
     },
   ];
 
   return (
-    <div className="flex flex-wrap justify-center gap-6 mt-20">
-      {cardsData.map((card, index) => (
-        <Card key={index} {...card} />
-      ))}
+    <div>
+      <Header />
+      <div className="flex flex-wrap justify-center gap-4 p-3 bg-stone-900">
+        {cardsData.map((card, index) => (
+          <div key={index} className="w-full sm:w-1/2 lg:w-1/4">
+            <Card {...card} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
